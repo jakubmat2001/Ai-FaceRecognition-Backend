f09c7c627d6580ec3660e2b2ede47318f448f6fa
jakubmat2001 <jakubmatusik11@gmail.com>
2023-08-16 21:41:33 +0100

    Added our backed from fullstack Ai-Web-recogn rep

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const knex = require('knex')
+const cors = require('cors')
+const bcrypt = require('bcryptjs');
+
+// Importing routes to the end-point req
+const register = require('./controller/register');
+const signin = require('./controller/signin');
+const profile = require('./controller/profile');
+const image = require('./controller/image');
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+// Establising connection with our database
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: 'lekcja11',
+        database: 'face-recogn-db'
+    }
+});
+
+app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.post("/signin", (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
+app.get("/profile/:id", (req, res) => {profile.handleProfile(req, res, db)});
+app.put("/image", (req, res) => { image.handleImage(req, res, db)});
+app.post("/imageurl", (req, res) => { image.handleImageURL(req, res)});
+
+
+app.listen(3001, () => {
+    console.log("app is running on: " + "http://localhost:3001/")
+});
+
