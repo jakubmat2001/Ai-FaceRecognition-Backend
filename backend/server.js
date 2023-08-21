d8389f20202f564d56c3eb585baa15fdece0d9ef
jakubmat2001 <jakubmatusik11@gmail.com>
2023-08-21 16:01:22 +0100

    yet another fix for db

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,6 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false
-          }
     }
 });
 
