80ec058f7fe2a760fcfd48f2ac294c0e7c0a5586
jakubmat2001 <jakubmatusik11@gmail.com>
2023-08-27 17:37:48 +0100

    debugging 1

--- controller/newPassword.js
+++ controller/newPassword.js
@@ -9,8 +9,12 @@ const handleChangePassword = (req, res, db, bcrypt) => {
     db.select('email', 'hash').from('login')
         .where('email', '=', req.body.email)
         .then(user => {
+            console.log(req.body.email)
+            console.log(req.body.password)
+            console.log(req.body.newPassword)
             const isValidPassword = bcrypt.compareSync(req.body.password, user[0].hash);
             const isNotSame = req.body.password === req.body.newPassword;
+            console.log(isNotSame)
             if (!isNotSame && isValidPassword) {
                 return db.select('login')
                     .where('email', '=', req.body.email)
@@ -20,7 +24,7 @@ const handleChangePassword = (req, res, db, bcrypt) => {
                     ).then(res.json("success"))
                     .catch(err => res.json("Failed to update user password"))
             } else {
-                res.status(400).json("Password entered was wrong, or you've entered the same password")
+                res.status(400).json("Password entered was wrong, or you've entered the same password" + req.body.password + req.body.newPassword)
             }
         }).catch(err => res.status("Failed to find a user, please log-in and try again"))
 };
