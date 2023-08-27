e87270313153a1a202a5ac153a3278f158cf193f
jakubmat2001 <jakubmatusik11@gmail.com>
2023-08-27 16:51:52 +0100

    Adding and testing user update password func

---
+++ controller/newPassword.js
@@ -0,0 +1,30 @@
+const handleChangePassword = (req, res, db, bcrypt) => {
+    const { email, password, newPassword } = req.body;
+    if (!password || !newPassword) {
+        return res.status(400).json("Enter your old and new passwords into form")
+    } else if (!email) {
+        return res.status(400).json("Email not found")
+    }
+    const newHash = bcrypt.hashSync(req.body.newPassword);
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(user => {
+            const isValidPassword = bcrypt.compareSync(req.body.password, user[0].hash);
+            const isNotSame = req.body.password === req.body.newPassword;
+            if (!isNotSame && isValidPassword) {
+                return db.select('login')
+                    .where('email', '=', req.body.email)
+                    .update({
+                        hash: newHash
+                    }
+                    ).then(res.json("success"))
+                    .catch(err => res.json("Failed to update user password"))
+            } else {
+                res.status(400).json("Password entered was wrong, or you've entered the same password")
+            }
+        }).catch(err => res.status("Failed to find a user, please log-in and try again"))
+};
+
+module.exports = {
+    handleChangePassword: handleChangePassword
+};
--- controller/register.js
+++ controller/register.js
@@ -7,7 +7,7 @@ const handleRegister = (req, res, db, bcrypt) => {
     const hash = bcrypt.hashSync(password); // Hash and store user entered password
     
     // Start a transaction which if fails, reverts all the changes made inside of it
-    // This method is similar to conducting bank transfer transactions where if all the contions are met
+    // This method is similar to conducting bank transfer transactions where if all the conditons are met
     // then all the code inside of the transactions exectures all together
     db.transaction(trx => {
         trx.insert({
