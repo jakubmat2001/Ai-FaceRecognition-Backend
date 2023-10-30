c6ea43da1771e196251d55a57b755bc868498585
jakubmat2001 <jakubmatusik11@gmail.com>
2023-10-30 23:45:39 +0000

    fix the issue with data not fetching

--- controller/signin.js
+++ controller/signin.js
@@ -1,13 +1,21 @@
 const jwt = require("jsonwebtoken");
 const redis = require("redis");
 
-// Redis db
-const redisClient = redis.createClient(process.env.REDIS_URI);
+// Redis db, prepared for production deployment :)
+const redisClient = redis.createClient({
+    url: 'redis://redis:6379',
+    legacyMode: true
+});
+
+async function redisConnect() {
+    return await redisClient.connect();
+}
+redisConnect()
 
 // Check if an existing users credentials match 
 const handleSignin = (db, bcrypt, req, res) => {
-    const {email, password} = req.body;
-    if (!email || !password){
+    const { email, password } = req.body;
+    if (!email || !password) {
         return Promise.reject("Form")
     }
     return db.select('email', 'hash').from('login')
@@ -17,7 +25,8 @@ const handleSignin = (db, bcrypt, req, res) => {
             if (isValidPassword) {
                 return db.select('*').from('users')
                     .where('email', '=', req.body.email) // If password is valid, then check if email matches
-                    .then(user =>  user[0]) // Return a user to the frontend if all conditions are met
+                    .then(user => user[0]) // Return a user to the frontend if all conditions are met
+                    
                     .catch(err => Promise.reject('Not Found'))
             } else {
                 Promise.reject("Password Not Matching")
@@ -25,42 +34,45 @@ const handleSignin = (db, bcrypt, req, res) => {
         }).catch(err => Promise.reject('Not Existing'))
 }
 
-const getAuthTokenId = () => {
-    console.log("signin authorized");
-    // return redisClient.get(authorization, (err, reply) => {
-    //     if (err || !reply){
-    //         return res.status(400).json("unathroized");
-    //     }
-    //     return res.json({id: reply})
-    // });
-}
-
-// const setToken = (token, id) => {
-//     return Promise.resolve(redisClient.set(token, id))
-// }
-
-const createSessions = async (user) => {
-    const {email, id } = user;
-    const token = signinToken(email)
-    return { success: "true", userID: id, token };
-}
-
 const signinToken = (email) => {
-    const jwtPayload = {email};
-    return jwt.sign(jwtPayload, 'JWT-SECRET', {expiresIn: "1h"});
+    const jwtPayload = { email };
+    return jwt.sign(jwtPayload, 'JWT-SECRET', { expiresIn: "1h" });
+}
+
+const getAuthTokenId = (req, res) => {
+    const { authorization } = req.headers;
+    return redisClient.GET(authorization, (err, reply) => {
+        if (err || !reply) {
+            return res.status(401).send("unauthorized");
+        }
+        return res.json({id: reply});
+    });
+}
+
+
+const setToken = (token, id) => {
+    return Promise.resolve(redisClient.SET(token, id))
+}
+
+const createSessions = (user) => {
+    const { email, id } = user;
+    const token = signinToken(email)
+    return setToken(token, id)
+        .then(() => { return { success: "true", userID: id, token } 
+    }).catch(console.log);
+        
 }
 
 const handleSigninAuth = (db, bcrypt) => (req, res) => {
     const { authorization } = req.headers;
-    return authorization ? getAuthTokenId() : handleSignin(db, bcrypt, req, res)
-    .then(data => {
-        return data.id && data.email ? createSessions(data) : Promise.reject(data)
-    })
-    .then(session => res.json(session))
-    .catch(err => res.status(400).json(err))
+    return authorization ? getAuthTokenId(req, res)
+        : handleSignin(db, bcrypt, req, res)
+            .then(data =>
+                data.id && data.email ? createSessions(data) : Promise.reject(data))
+            .then(session => res.json(session))
+            .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
-    handleSigninAuth: handleSigninAuth,
-    handleSignin: handleSignin
+    handleSigninAuth: handleSigninAuth
 }
